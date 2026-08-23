203efa422a4be02ea7e33d44463acc42dfb9212a
V. Prescott <victorprescott554@gmail.com>
2026-08-23 10:21:07 +0530

    feat: add scroller configs

---
+++ labs-briarcastle/src/components/Scroll.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
+gsap.registerPlugin(ScrollTrigger)
+
+export default function LenisProvider() {
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2,
+            smoothWheel: true,
+        });
+
+        lenis.on("scroll", ScrollTrigger.update);
+
+        const update = (time: number) => {
+            lenis.raf(time * 1000);
+        }
+
+        gsap.ticker.add(update);
+
+        return () => {
+            gsap.ticker.remove(update);
+            lenis.destroy();
+        }
+    }, []);
+    
+    return null;
+}
